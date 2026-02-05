5a11a9a424a8e96b64aced93c8fe4b447c296e6f
Grzegorz Rzeznikiewicz <grzegorzrzeznikiewicz@gmail.com>
2026-02-05 11:53:24 +0100

    Submodule 9.4: Add booking page with navigation system and table reservation widget

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,60 @@
-import {select, settings} from './settings.js';
+import {classNames, select, settings} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
 
@@ -38,14 +90,21 @@ const app = {
         /* execute initMenu method */
         thisApp.initMenu();
       });
+  },
+  appInitBooking: function () {
+    const thisApp = this;
 
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
   },
   init: function () {
     const thisApp = this;
 
+    thisApp.initPages();
+
     thisApp.initData();
     thisApp.initCart();
+    thisApp.appInitBooking();
   },
 };
 
